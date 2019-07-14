8181e0f72354286d6f6d3e77fef96c349685c6bb
wuqiqi <wuqiqi@qq.com>
2019-07-14 16:37:55 +0800

    update react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Login from './pages/login/login'
+//import Admin from './pages/admin/admin'
+
+export default class App extends Component {
+    render() {
+        return (             
+            <div>
+               <Login/>     
+            </div>
+        )
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+//import {Router,HashRouter} from 'react-router-dom'
+import App from './App'
+
+ReactDOM.render(<App/>,document.getElementById('root'))
